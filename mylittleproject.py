d5a350c60bd792be3e6c1a41665e5187f27dbe15
nikitadovzhik <nickdovzhik1995@gmail.com>
2022-10-28 17:55:14 +0300

    create file mylittleproject

--- mylittleproject.py
+++ mylittleproject.py
@@ -1,1 +1,38 @@
-"12345"
+per1 = "Hello!" #str
+per2 = "It's third my homework." #str
+print(per1, per2)
+per3 = "I hope I did it right)" #str
+print(per3)
+per4 = 555097 #int
+per5 = 574856 #int
+per6 = 11223344 #int
+print(per4,per5,per6)
+per7 = 555.345 #float
+per8 = 1.345 #float
+per9 = 125.987 #float
+print(per7,per8,per9)
+per10 = True #boolean
+per11 = False #boolean
+print(per10, per11)
+per13 = None #Пустое значение
+per14 = None #Пустое значение
+print(per13,per14)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
